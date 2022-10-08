bfa822df3fd6cc3480054ac56585ea8e836727d7
adamalcala <adamalcala1988@gmail.com>
2022-10-08 12:07:09 -0700

    condensed code

--- gather-information.sh
+++ gather-information.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
+line="---------------------------------------"
+echo "Starting at: $(date)"; echo $line
 
-echo "Starting at: $(date)"
-echo
+echo "UPTIME"; uptime; echo $line
 
-echo "UPTIME"
-uptime
-echo
+echo "FREE"; free; echo $line
 
-echo "FREE"
-free
-echo
-
-echo "WHO"
-who
-echo
+echo "WHO"; who; echo $line
 
 echo "Finishing at: $(date)"
