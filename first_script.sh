ec2a964fc08a513fdd105cd461c53b4de3ad7ae9
mellowc0de <adamalcala1988@gmail.com>
2023-04-29 03:16:24 -0700

    Create first_script.sh

---
+++ first_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Author:
+# Date Created:
+# Last Modified:
+
+# Description
+# Prints "This is my first script" to the terminal
+
+# Usage
+# first_script
+
+echo "This is the first script"
+exit 0
