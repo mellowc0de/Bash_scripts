19ae907503a8a4ab1bf2250f4e2561f7e6ff244b
mellowc0de <adamalcala1988@gmail.com>
2023-04-29 03:17:24 -0700

    Update first_script.sh

--- first_script.sh
+++ first_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Author:
-# Date Created:
-# Last Modified:
+# Author:         Adam Ray Alcala
+# Date Created:   04/29/2023
+# Last Modified:  04/29/2023
 
 # Description
 # Prints "This is my first script" to the terminal
